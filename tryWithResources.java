b0ec6b885303e17baf45a93551c9c717b5508f15
Kaleab <39136564+kalom60@users.noreply.github.com>
2023-06-16 13:24:43 +0300

    Update tryWithResources.java

--- tryWithResources.java
+++ tryWithResources.java
@@ -3,11 +3,6 @@ import java.io.FileReader;
 
 public class tryWithResources {
     public static void main(String[] args) {
-        try(BufferedReader br = new BufferedReader(
-                new FileReader("/Users/rollin/Github/safaricometCode_Challenge\n"))) {
-            String line = br.readLine();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        
     }
 }
